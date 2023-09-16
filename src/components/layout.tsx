55ee2981fa771eff87b43a2f3eb27c2ef4e1cf06
Luke Turner <github@luketurner.org>
2023-09-15 19:16:19 -0700

    move everything under src

---
+++ src/components/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Dust',
+  description: 'Task management app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
