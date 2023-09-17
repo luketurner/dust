6675293950b7c0317156b464a772f1afe93cd8d1
Luke Turner <github@luketurner.org>
2023-09-16 23:00:37 -0700

    add helper functions for authz

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,14 @@
 import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
 import type { NextAuthOptions as NextAuthConfig } from "next-auth"
-import { getServerSession } from "next-auth"
+import { getServerSession } from "next-auth/next"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import GitHub from "next-auth/providers/github";
 
 import { prisma } from "@/db/client";
+
+import { Prisma } from "@prisma/client";
+import { redirect } from "next/navigation"
 
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 declare module "next-auth/jwt" {
@@ -22,7 +25,7 @@ export const config = {
     GitHub({ clientId: process.env.AUTH_GITHUB_ID, clientSecret: process.env.AUTH_GITHUB_SECRET }),
   ],
   callbacks: {
-    async jwt({ token }) {
+    async jwt({ token }: any) {
       token.userRole = "admin"
       return token
     },
@@ -33,6 +36,40 @@ export const config = {
 // https://next-auth.js.org/configuration/nextjs#getserversession
 export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
   return getServerSession(...args, config)
+}
+
+export async function getServerUser() {
+  const session = await auth();
+
+  if (!session?.user?.email) {
+    return { session };
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    },
+  });
+
+  return { user, session }
+}
+
+export async function getServerUserOrThrow() {
+  const { session, user } = await getServerUser();
+  if (!user || !session) {
+    throw new Error('Unauthorized')
+  }
+
+  return { session, user };
+}
+
+export async function getServerUserOrRedirect(url?: string) {
+  const { session, user } = await getServerUser();
+  if (!user || !session) {
+    redirect(url ?? '/');
+  }
+
+  return { session, user };
 }
 
 // We recommend doing your own environment variable validation
