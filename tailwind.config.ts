3558212d8fc9c211874900c760222545c581b658
Luke Turner <github@luketurner.org>
2023-09-17 18:23:16 -0700

    add tailwindcss-react-aria-components plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {},
-  plugins: [],
+  plugins: [
+    require('tailwindcss-react-aria-components')
+  ],
 }
 export default config
