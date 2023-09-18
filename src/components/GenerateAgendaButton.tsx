95a29cef6a8a981399768f2d12dae25c0ae93acf
Luke Turner <github@luketurner.org>
2023-09-17 19:33:44 -0700

    fix generate button

--- src/components/GenerateAgendaButton.tsx
+++ src/components/GenerateAgendaButton.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { upsertAgendaAction } from "@/actions/agenda";
+import { upsertAgenda } from "@/actions/agenda";
 import { useRouter } from "next/navigation";
 
 export default function GenerateAgendaButton({ date }: { date: string }) {
   const router = useRouter();
   return (
-    <button onClick={async () => { await upsertAgendaAction(date); router.refresh(); }}>
+    <button onClick={async () => { await upsertAgenda(date); router.refresh(); }}>
       Generate Agenda
     </button>
   )
