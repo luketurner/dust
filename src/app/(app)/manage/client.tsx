b5da946e68b716e3d183ad3837ee8571d45a00a5
Luke Turner <github@luketurner.org>
2023-10-01 17:54:48 -0700

    add tag editing to manage page

--- src/app/(app)/manage/client.tsx
+++ src/app/(app)/manage/client.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Tag as TagType, Task as TaskType } from "@prisma/client";
-import { Key, useCallback, useMemo, useState } from "react";
-import { Button, Checkbox, GridList, Item, TagGroup, TagList } from "react-aria-components";
-import TaskEntry from "@/components/TaskEntry";
-import Task from "@/components/Task";
-import { addTasksFromText, removeTags } from "@/actions/task";
-import Tag from "@/components/Tag";
+import { Tag, Tag as TagType, Task as TaskType } from "@prisma/client";
+import { Key, useCallback } from "react";
+import AppLayout from "@/components/AppLayout";
+import { ServerErrorAction, useClientServerReducer } from "@/hooks/clientServerReducer";
+import { ActionButton, ActionMenu, Item, ListView, Selection, Text } from "@adobe/react-spectrum";
+import EditTagDialog from "@/components/EditTagDialog";
+import { createTag, deleteTag, updateTag } from "@/actions/tag";
+import { ToastQueue } from "@react-spectrum/toast";
 
 type TaskWithTags = TaskType & { tags: TagType[] };
 
@@ -15,133 +16,122 @@ export interface TaskManagerProps {
   tags: TagType[]
 }
 
-interface Filters {
-  active: boolean;
-  archived: boolean;
-  tags: TagType[];
+interface ManagePageClientState {
+  tasks: TaskWithTags[]
+  tags: TagType[]
+  selectedTags: string[]
+  dialog?: {
+    type: 'add-tag' | 'edit-tag'
+    tagId?: string
+  }
+}
+
+interface SelectTagsAction { type: 'select-tags'; tags: Set<Key> | 'all'; }
+interface OpenAddTagAction { type: 'open-add-tag'; }
+interface OpenEditTagAction { type: 'open-edit-tag'; tagId: string; }
+interface AddTagAction { type: 'add-tag'; data: Partial<Tag>; }
+interface AddTagFinishedAction { type: 'add-tag-finished'; tag: Tag; }
+interface EditTagAction { type: 'edit-tag'; tagId: string, data: Partial<Tag>; }
+interface DeleteTagAction { type: 'delete-tag'; tagId: string; }
+interface CloseDialogAction { type: 'close-dialog'; }
+
+type ManagePageClientAction = ServerErrorAction | SelectTagsAction | OpenAddTagAction | OpenEditTagAction | AddTagAction | EditTagAction | DeleteTagAction | AddTagFinishedAction | CloseDialogAction;
+
+function clientReducer(state: ManagePageClientState, action: ManagePageClientAction) {
+  switch (action.type) {
+    case 'select-tags':
+      state.selectedTags = action.tags === 'all' ? state.tags.map(({ id }) => id) : Array.from(action.tags as Set<string>)
+      break;
+    case 'open-add-tag':
+      state.dialog = { type: 'add-tag' }
+      break;
+    case 'open-edit-tag':
+      state.dialog = { type: 'edit-tag', tagId: action.tagId }
+      break;
+    case 'add-tag-finished':
+      state.tags.push(action.tag);
+      delete state.dialog;
+      break;
+    case 'edit-tag':
+      const tag = state.tags.find(({ id }) => id === action.tagId)
+      if (tag && typeof action.data.name === 'string') tag.name = action.data.name;
+      delete state.dialog;
+      break;
+    case 'delete-tag':
+      state.tags = state.tags.filter((tag) => tag.id !== action.tagId);
+      state.selectedTags = state.selectedTags.filter((id) => id !== action.tagId);
+      break;
+    case 'close-dialog':
+      delete state.dialog;
+      break;
+    case 'server-error':
+      ToastQueue.negative('Error: ' + (action.error as Error)?.message ?? 'Unknown error');
+      break;
+  }
+}
+
+async function serverReducer(action: ManagePageClientAction) {
+  switch (action.type) {
+    case 'add-tag':
+      const tag = await createTag(action.data);
+      return { type: 'add-tag-finished', tag };
+    case 'edit-tag':
+      await updateTag(action.tagId, action.data)
+      break;
+    case 'delete-tag':
+      await deleteTag(action.tagId);
+      break;
+  }
 }
 
 export default function ManagePageClient({ tasks: initialTasks, tags: initialTags }: TaskManagerProps) {
-  const [tasks, setTasks] = useState(initialTasks);
-  const [allTags, setAllTags] = useState(initialTags);
-  const [filters, setFilters] = useState<Filters>({ active: true, archived: false, tags: [] });
 
-  const filteredTasks = useMemo(() => {
-    return tasks.filter(task => {
-      if (filters.active && !task.archived) { return true; }
-      if (filters.archived && task.archived) { return true; }
-      return false;
-    })
-  }, [tasks, filters]);
+  const [state, dispatch] = useClientServerReducer<ManagePageClientState, ManagePageClientAction>(clientReducer, serverReducer, {
+    tasks: initialTasks,
+    tags: initialTags,
+    selectedTags: [],
+  });
 
-  const taskEntrySubmit = useCallback(async (v: string) => {
-    const newTasks = await addTasksFromText(v);
-    // TODO -- needs to update tags as well
-    setTasks([...tasks, ...newTasks].sort((a, b) => a.displayOrder - b.displayOrder));
-  }, [tasks]);
+  const handleTagSelectionChange = useCallback((keys: Selection) => {
+    dispatch({ type: 'select-tags', tags: keys })
+  }, []);
 
-  const handleRemoveTags = useCallback(async (taskId: string, tagIds: Set<Key>) => {
-    const tagIdList = Array.from(tagIds) as string[];
-    const { deletedTagIds } = await removeTags(taskId, tagIdList);
+  const handleAddTag = useCallback(() => {
+    dispatch({ type: 'open-add-tag' })
+  }, []);
 
-    setTasks(tasks.map(task => {
-      if (task.id === taskId) {
-        return { ...task, tags: task.tags.filter(tag => !tagIdList.includes(tag.id)) };
-      }
-      return task;
-    }));
-
-    if (deletedTagIds.length > 0) {
-      setAllTags(allTags.filter(tag => !deletedTagIds.includes(tag.id)));
+  const handleTagMenuAction = useCallback((tagId: string, key: Key) => {
+    switch (key) {
+      case 'edit':
+        dispatch({ type: 'open-edit-tag', tagId })
+        break;
+      case 'delete':
+        dispatch({ type: 'delete-tag', tagId })
+        break;
     }
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-row flex-wrap">
-        <TaskStatusFilters filters={filters} onChange={(v) => setFilters(v)} />
-        <TaskTagFilters onChange={(v) => setFilters({ ...filters,tags: v })} tags={allTags} selectedTags={filters.tags} />
-      </div>
-      <GridList className="my-2 ml-2 flex flex-col flex-nowrap w-full" selectionMode="multiple" items={filteredTasks}>
-        {(task) => (
-          <Item id={task.id} textValue={task.name}>
-            <div className="w-full">
-              <div className="w-full flex flex-row flex-nowrap">
-                <Task task={task} key={task.id} onChange={(newTask) => {setTasks([...tasks])}} onDelete={() => setTasks(tasks.filter((t) => t.id !== task.id))} />
-              </div>
-              <div className="w-full flex flex-row flex-wrap">
-                <TagGroup onRemove={(tagIds) => handleRemoveTags(task.id, tagIds)}>
-                  <TagList items={task.tags}>
-                    {(tag) => (
-                      <Tag textValue={tag.name}>
-                        {tag.name}
-                      </Tag>
-                    )}
-                  </TagList>
-                </TagGroup>
-              </div>
-            </div>
+    <AppLayout user={true} breadcrumbs={[{ label: 'Manage', url: '/manage', key: 'manage' }]}>
+      <EditTagDialog
+       onSave={(tagId, data) => dispatch(tagId ? { type: 'edit-tag', tagId, data } : { type: 'add-tag', data })}
+       onClose={() => dispatch({ type: 'close-dialog' })}
+       tag={state.tags.find(({id}) => id === state.dialog?.tagId)}
+       isOpen={state.dialog?.type === 'add-tag' || state.dialog?.type === 'edit-tag'}
+       isCreate={state.dialog?.type === 'add-tag'}
+       />
+      <ListView items={state.tags} selectionMode="multiple" aria-label="List of selected tags" onSelectionChange={handleTagSelectionChange}>
+        {(tag) => (
+          <Item key={tag.id} textValue={tag.name}>
+            <Text>{tag.name}</Text>
+            <ActionMenu onAction={(key) => handleTagMenuAction(tag.id, key)}>
+              <Item key="edit">Edit...</Item>
+              <Item key="delete">Delete</Item>
+            </ActionMenu>
           </Item>
         )}
-      </GridList>
-      <TaskEntry onSubmit={taskEntrySubmit} />
-    </div>
-  )
-}
-
-function TaskStatusFilters({ onChange, filters }: { 
-  onChange(v: Filters): void
-  filters: Filters
-}) {
-  const toggleActive = useCallback(() => onChange({ ...filters, active: !filters.active }), [onChange, filters])
-  const toggleArchived = useCallback(() => onChange({ ...filters, archived: !filters.archived }), [onChange, filters])
-  return (
-    <div className="flex flex-row flex-wrap w-100 mb-8 mx-2">
-      <div className="flex flex-col flex-wrap">
-        <p className="font-bold">Status filters</p>
-        <Checkbox className="ml-2 m-1 hover:underline cursor-pointer" isSelected={filters.active} onChange={toggleActive}>
-          {filters.active ? 'hide' : 'show'} active
-        </Checkbox>
-        <Checkbox className="ml-2 m-1 hover:underline cursor-pointer" isSelected={filters.archived} onChange={toggleArchived}>
-          {filters.archived ? 'hide' : 'show'} archived
-        </Checkbox>
-      </div>
-    </div>
-  )
-}
-
-function TaskTagFilters({ onChange, tags, selectedTags }: { 
-  onChange(v: TagType[]): void
-  selectedTags: TagType[]
-  tags: TagType[]
-}) {
-  // const allTags: { [key: string]: Tag } = {};
-  // for (const task of tasks) {
-  //   for (const tag of task.tags ?? []) {
-  //     allTags[tag.id] = tag;
-  //   }
-  // }
-  const selectedTagIds = new Set(selectedTags.map(t => t.id));
-  const toggleTag = useCallback((id: string) => {
-    if (selectedTagIds.has(id)) {
-      return onChange(selectedTags.filter((tag) => tag.id !== id));
-    } else {
-      const tag = tags.find(tag => tag.id === id);
-      if (!tag) return;
-      return onChange([...selectedTags, tag]);
-    }
-  }, [onChange, selectedTags, tags]);
-
-  return (
-    <div className="flex flex-row flex-wrap w-100 mb-8 mx-2">
-      <div className="flex flex-col flex-wrap">
-        <p className="font-bold">Tag filters</p>
-        {tags.map(tag => (
-          <Checkbox key={tag.id} className="ml-2 m-1 hover:underline cursor-pointer" isSelected={selectedTagIds.has(tag.id)} onChange={() => toggleTag(tag.id)}>
-            {selectedTagIds.has(tag.id) ? '[x]' : '[ ]'} {tag.name}
-          </Checkbox>
-        ))}
-      </div>
-    </div>
-  )
+      </ListView>
+      <ActionButton onPress={handleAddTag}>Add tag...</ActionButton>
+    </AppLayout>
+  );
 }
---
+++ src/components/EditTagDialog.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Button, ButtonGroup, Content, Dialog, DialogContainer, Form, Heading, TextField, ComboBox, Item, Picker, TagGroup, TextArea, Checkbox } from "@adobe/react-spectrum";
+import { Tag } from "@prisma/client";
+import { useCallback, useMemo } from "react";
+import { useImmer } from "use-immer";
+
+export interface EditTagDialogData {
+  name: string;
+}
+
+export interface EditTagDialogProps {
+  isOpen?: boolean;
+  isCreate?: boolean;
+  tag?: Tag | null | undefined
+  onClose(): void
+  onSave(tagId: string | null, data: EditTagDialogData): void
+}
+
+export default function EditTagDialog({ tag, onClose, onSave, isOpen, isCreate }: EditTagDialogProps) {
+  return (
+    <DialogContainer onDismiss={onClose}>
+      {isOpen && <EditTagDialogInner key={isCreate ? 'create' : tag!.id} tag={tag} onClose={onClose} onSave={onSave} isCreate={isCreate} />}
+    </DialogContainer>
+  )
+}
+
+function EditTagDialogInner({ tag, onClose, onSave, isCreate }: EditTagDialogProps) {
+
+  const [data, setData] = useImmer<EditTagDialogData>({
+    name: tag?.name ?? "",
+  });
+
+  const handleNameChange = useCallback((value: string) => {
+    setData(draft => {
+      draft.name = value;
+    })
+  }, []);
+
+  const handleSave = useCallback(() => {
+    onSave(tag?.id ?? null, data)
+  }, [onSave, tag, data]);
+
+  return (
+    <Dialog>
+      <Heading>{isCreate ? 'Add' : 'Edit'} tag</Heading>
+      <Content>
+      <Form maxWidth="size-3600">
+        <TextField label="Name" value={data.name} onChange={handleNameChange} />
+      </Form>
+      </Content>
+      <ButtonGroup>
+        <Button variant="secondary" onPress={onClose}>Cancel</Button>
+        <Button variant="accent" onPress={handleSave}>Save</Button>
+      </ButtonGroup>
+    </Dialog>
+  )
+}
