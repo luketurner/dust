1b7090fd0823f3ee6d8398119c6f3382600ed81c
Luke Turner <github@luketurner.org>
2023-10-01 17:49:56 -0700

    add server actions for tags

---
+++ src/actions/tag.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { getServerUserOrThrow } from "@/auth";
+import { prisma } from "@/db/client";
+import { Tag } from "@prisma/client";
+
+export async function deleteTag(id: string): Promise<void> {
+  const { user } = await getServerUserOrThrow();
+  await prisma.tag.delete({
+    where: {
+      userId: user.id,
+      id
+    }
+  });
+}
+
+export async function updateTag(id: string, data: {
+  name?: string
+}): Promise<void> {
+  const { user } = await getServerUserOrThrow();
+  await prisma.tag.update({
+    where: {
+      id,
+      userId: user.id
+    },
+    data: {
+      name: data.name,
+    }
+  });
+}
+
+export async function createTag(data: {
+  name?: string;
+}): Promise<Tag> {
+  const { user } = await getServerUserOrThrow();
+  
+  return await prisma.tag.create({
+    data: {
+      userId: user.id,
+      name: data.name ?? 'Unnamed tag',
+    }
+  });
+}
