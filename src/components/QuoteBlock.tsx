d11c8aa6694164ba5d9b1c2c53bbed95aea6711f
Luke Turner <github@luketurner.org>
2023-10-15 13:31:02 -0700

    adjust quote rendering

--- src/components/QuoteBlock.tsx
+++ src/components/QuoteBlock.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { View } from "@adobe/react-spectrum";
 import { Quote } from "@prisma/client"
 
 export interface QuoteProps {
@@ -8,13 +9,13 @@ export interface QuoteProps {
 
 export default function QuoteBlock({ quote }: QuoteProps) {
   return (
-    <div>
-      <pre style={{ font: 'inherit' }}>
+    <View>
+      <pre style={{ font: 'inherit', whiteSpace: 'pre-line' }}>
         {quote.content}
       </pre>
-      <p className="text-right" style={{maxWidth: '10rem'}}>
-        {quote.author}
+      <p className="text-right italic" style={{maxWidth: '16rem'}}>
+        {quote.author} ({quote.source})
       </p>
-    </div>
+    </View>
   )
 }
