95eb09082a31040d44a55b1a15720c5e383286a7
Luke Turner <github@luketurner.org>
2023-10-17 21:43:23 -0700

    update homepage text

--- src/app/(no-chrome)/client.tsx
+++ src/app/(no-chrome)/client.tsx
@@ -24,10 +24,17 @@ export default function IndexPageClient({ quote }: IndexPageClientProps) {
         <View gridArea="b" justifySelf={{base: 'center', 'M': 'end'}}>
           <QuoteBlock quote={quote} />
         </View>
-        <Flex gridArea="c" direction="column" marginTop={{base: '0', 'M': "single-line-height"}} marginX={{ base: 'auto', 'M': 0 }} gap="single-line-height">
-          <p className="text-lg text-center">Task management for people who don&apos;t like tasks.</p>
+        <Flex gridArea="c" direction="column" marginTop={{base: '0', 'M': "single-line-height"}} marginX={{ base: 'auto', 'M': 0 }} >
+          <View UNSAFE_className="text-lg text-center" marginBottom="size-100">Task management for people who don&apos;t like tasks.</View>
+          <View elementType="ul" UNSAFE_className="text-center" marginBottom="single-line-height">
+            <li>Add all your tasks.</li>
+            <li>Every day, Dust picks 3 tasks for you to do.</li>
+            <li>Urgent/Important tasks are picked first.</li>
+            <li>Someday/Maybe tasks are picked last.</li>
+            <li>Automatically sync task data with Git repo(s).</li>
+          </View>
           <TinyAgendaPage date={DateTime.now().toISODate()!} quote={quote} />
-          <Button alignSelf="center" variant="accent" onPress={(e) => signIn('github')}>
+          <Button alignSelf="center" variant="accent" onPress={(e) => signIn('github')} marginTop="single-line-height">
             Log in with Github
           </Button>
         </Flex>
