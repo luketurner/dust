113a32e3e08b37ae9c0602c7917fda5150a765be
Luke Turner <github@luketurner.org>
2023-10-18 16:53:00 -0700

    tweak margins

--- src/components/TinyDemoPage.tsx
+++ src/components/TinyDemoPage.tsx
@@ -3,7 +3,7 @@ import { View } from "@adobe/react-spectrum";
 
 export default function TinyDemoPage({ children }: { children: React.ReactNode }) {
   return (
-    <View marginY={{ base: '-40%', M: '-20%' }}>
+    <View marginTop={{ base: '-32%', M: '-20%' }} marginBottom={{ base: '-30%', M: '-18%' }}>
       <div style={{ transform: 'scale(0.5, 0.5) rotate(-2deg) skew(2deg)' }} className="border-2 rounded shadow-xl">
         <Embedded>
           {children}
