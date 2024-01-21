02361b3e2e29161a70d8b67d4454256e0119d667
Luke Turner <github@luketurner.org>
2024-01-21 12:11:23 -0800

    split LLM version and embedding verson, just in case

--- src/serverConfig.ts
+++ src/serverConfig.ts
@@ -4,3 +4,4 @@ export const SSH_KEY_PASSPHRASE = process.env.DUST_SSH_KEY_PASSPHRASE || 'foobar
 
 export const LLM_SERVER = process.env.DUST_LLM_SERVER;
 export const LLM_VERSION = process.env.DUST_LLM_VERSION || 'test';
+export const EMBEDDING_VERSION = process.env.DUST_LLM_VERSION || 'test';
